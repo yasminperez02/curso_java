0e52bf05b2a84819c5b673310345431ee468e142
yasminperez02 <yasminrafaelaramires@gmail.com>
2026-03-06 15:44:21 -0300

    funções em expressões maiores.

--- entrada-de-dados/src/Main2.java
+++ entrada-de-dados/src/Main2.java
@@ -2,7 +2,7 @@ void main() {
     double x = 2.0;
     double y = 3.0;
     double z = -5.0;
-    double A, B, C;
+    double A, B, C, delta;
 
     A = Math.sqrt(x);
     B = Math.sqrt(y);
@@ -25,4 +25,17 @@ void main() {
     B = Math.abs(z);
     IO.println("valor absoluto de " + y + " é " + A);
     IO.println("valor absoluto de " + z + " é " + B);
+
+    double x1, x2;
+    double a, b, c;
+    a = 2.0;
+    b = 3.0;
+    c = 3.0;
+
+//    delta = Math.pow(b, 2.0) -4*a*c;
+//    x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+//    x2 = (-b -Math.sqrt(delta)) / (2.0 * a);
+//
+//    System.out.println(x1);
+//    System.out.println(x2);
 }
