a2bd5e47c035d2d3f5c521ef476801f0d22ed4af
yasminperez02 <yasminrafaelaramires@gmail.com>
2026-03-09 21:11:25 -0300

    estrutura do Exercicio8

---
+++ estrutura-condicional/src/Exercicios/Exercicio8.java
@@ -0,0 +1,14 @@
+package Exercicios;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double salario = sc.nextDouble();
+        double imposto;
+
+    }
+}
